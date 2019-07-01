a8c4a2f725b7e77fbc1accd661799094d2e4414c
Pine Mizune <pinemz@gmail.com>
2019-07-01 20:57:49 +0900

    feat: push to github

--- .circleci/update-remote.sh
+++ .circleci/update-remote.sh
@@ -2,6 +2,11 @@
 
 if [ -n "$(git --no-pager diff --raw)" ]; then
   echo 'The worktree is dirty.'
+
+  git config user.name 'Emoji Generator'
+  git config user.email 'ultimate.emoji.gen@gmail.com'
+  git config remote.origin 'git@github.com:emoji-gen/docker-node-py23.git'
+
   git add .
   git commit -m 'chore(Dockerfile): update from images'
   git push origin master
